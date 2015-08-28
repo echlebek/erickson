0a0eb1cdb3ad422b25dcef0c92d3dd40c26df2eb
Eric Chlebek <echlebek@gmail.com>
2015-08-28 12:18:37 -0700

    Add controls for review submission. Fix HTML bugs.

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,20 @@
 function createReview() {
   $("#create-review").removeClass("hidden");
+  $("#cancel-button").removeClass("hidden");
 }
 
 function cancelReview() {
   $("#create-review").addClass("hidden");
+  $("#cancel-button").addClass("hidden");
+}
+
+function submitReview() {
+}
+
+function pasteFile(file) {
+  var reader = new FileReader();
+  reader.onload = function(e) {
+    $("#diff").text(e.target.result);
+  }
+  reader.readAsText(file);
 }
